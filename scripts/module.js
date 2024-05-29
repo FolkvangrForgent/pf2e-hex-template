38967a01af3b8d4b517661e632c24a62c0d820f1
FolkvangrForgent <25625121+FolkvangrForgent@users.noreply.github.com>
2024-05-29 11:12:04 -0400

    Initial Module

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,11 @@
-Hooks.once('init', async function() {
-
-});
-
-Hooks.once('ready', async function() {
-
+Hooks.on('canvasInit', async function (canvas) {
+  if (canvas.grid.type === CONST.GRID_TYPES.HEXODDR || canvas.grid.type === CONST.GRID_TYPES.HEXEVENR || canvas.grid.type === CONST.GRID_TYPES.HEXODDQ || canvas.grid.type === CONST.GRID_TYPES.HEXEVENQ ) {
+    if (!game.settings.get("core", "gridTemplates")) {
+      await game.settings.set("core", "gridTemplates", true);
+    }
+  } else {
+    if (game.settings.get("core", "gridTemplates")) {
+      await game.settings.set("core", "gridTemplates", false);
+    }
+  }
 });
